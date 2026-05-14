00da119015184cd341e7bbda0a986ca069dd8b29
tharun-0909 <tharungandla2006@gmail.com>
2026-05-14 13:46:41 +0530

    Delete day3ass.js directory

--- day3ass.js/task.js
+++
@@ -1,7 +0,0 @@
-import { validatetasktitle,validatepriority,validateduedate} from "./validator.js";
-// 1. Add new task
-                    function addTask(title, priority, dueDate) {
-                      if(!titel && !priority && !duedate){
-                        
-                      }
-                    }
