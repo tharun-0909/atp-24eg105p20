e11a7a74d05b8e311bc60f8111f7fc8f5f716d31
tharun-0909 <tharungandla2006@gmail.com>
2026-05-21 15:35:46 +0530

    Delete asynchronous.js

--- asynchronous.js
+++
@@ -1,8 +0,0 @@
-console.log("OTP sending sucessfully");
-function otp(){
-    console.log("resend OTP");
-}
-let intervalId=setInterval(otp,2000)
-setTimeout(() => {
-    clearInterval(intervalId)
-},10000);
